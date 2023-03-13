d9736d68745bdad4690c03b901bb7c59941a08b8
guno517 <guno5170@gmail.com>
2023-03-13 16:44:38 +0900

    test1

--- src/api/core.ts
+++ src/api/core.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const { VITE_BASE_URL } = import.meta.env;
 const API_BASE_URL =
-  import.meta.env.MODE === 'development' ? VITE_BASE_URL : '/api';
+  import.meta.env.MODE === 'development' ? VITE_BASE_URL : '';
 
 const axiosApi = (options = {}) => {
   const instance = axios.create({ baseURL: API_BASE_URL, ...options });
